d02631e2ef1cfef02a166ac31ec663b66607595d
Lourdes Monahan <lourdes.marija.monahan@gmail.com>
2019-07-27 11:10:51 -0700

    delete button works

--- scripts.js
+++ scripts.js
@@ -7,7 +7,15 @@ let customers = [
   { id: 4, name: 'shep', email: 'shep@gmail.com'},
  ];
 
-customerList.addEventListener()
+customerList.addEventListener('click', (event) => {
+  if(event.target.tagName === 'BUTTON') {
+    const idx= [...event.target.parentNode.children].indexOf(event.target);
+    customers = customers.filter((customer, _index) => {
+      return _index !== idx;
+    });
+    render();
+  }
+});
 
 const render = () => {
   const html = customers.map((customer, idx) => {
