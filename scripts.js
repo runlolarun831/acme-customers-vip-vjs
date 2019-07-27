19f1616ac16abb04152092d5aa8d40a972df795d
Arthur <arthurcs426@gmail.com>
2019-07-27 10:46:05 -0700

    rendering list of customers

---
+++ scripts.js
@@ -0,0 +1,22 @@
+const customerList = document.querySelector('#customerList');
+
+
+let customers = [
+  { id: 1, name: 'moe', email: 'moe@gmail.com', isVIP: true},
+  { id: 2, name: 'larry', isVIP: true, email: 'larry@gmail.com'},
+  { id: 4, name: 'shep', email: 'shep@gmail.com'},
+ ];
+
+const render = () => {
+  const html = customers.map((customer, idx) => {
+    return `
+      <li idx='${idx}'><div>
+      ${customer.name} (${customer.email})
+      <button>Destroy</button>
+      </div></li>
+    `
+  }).join('');
+  customerList.innerHTML = html;
+};
+
+render();
